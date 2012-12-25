7eb8893c27b6617ceb9ef65f3794bd5ddc3433fc
Hilario J. Montoliu <hmontoliu@gmail.com>
2012-12-25 21:18:54 +0100

    Run all the natas py files in order

---
+++ runme.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# vim:ts=4:sw=4:et:ft=sh
+# runs all the natas py files 
+# Created: 2012-12-25
+
+# Copyright (c) 2012: Hilario J. Montoliu <hmontoliu@gmail.com>
+ 
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+# the full text of the license.
+
+
+runall () 
+{ 
+    for file in natas?.py natas??.py;
+    do
+        python $file 2>/dev/null | tee -a results.txt;
+    done
+}
+
+runall
