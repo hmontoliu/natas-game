f21deeb1b45834f534609596ee4ef7578a62feb4
Hilario J. Montoliu <hmontoliu@gmail.com>
2012-12-25 21:49:47 +0100

    license

--- libnatas.py
+++ libnatas.py
@@ -5,10 +5,17 @@
 libnatas.py
 
 clases y funciones comunes a todos los niveles
+
+Created: 2012-12-24
+
+Copyright (c) 2012: Hilario J. Montoliu  <hmontoliu@gmail.com> 
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+the full text of the license.
 """
-
-# http://wwwsearch.sourceforge.net/mechanize/
-
 import mechanize
 import re
 from BeautifulSoup import BeautifulSoup
