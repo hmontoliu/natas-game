386beff38fc4cefeb82f70cb43172ae71d882817
Hilario J. Montoliu <hmontoliu@gmail.com>
2012-12-25 12:50:56 +0100

    Subir la version de libnatas de este archivo; por error habia subido el antiguo

--- natas6.py
+++ natas6.py
@@ -7,54 +7,32 @@ natas6.py
 el presunto codigo fuente de la pagina muestra que el formulario de validacion necesita el valor 
 que aparece en includes/secret.inc
 """
+import re
+import libnatas
 
-# http://wwwsearch.sourceforge.net/mechanize/
-
-import re, sys
-import mechanize
-from BeautifulSoup import BeautifulSoup
-
-url = "http://natas6.natas.labs.overthewire.org"
-user = "natas6"
+# url: "http://natas6.natas.labs.overthewire.org"
+# user: "natas6"
 password = "mfPYpp1UBKKsx7g4F0LaRjhKKenYAOqU"
 
-nextlevel = int(user[-1:]) + 1
-
-
-br = mechanize.Browser()
-br.set_handle_referer(False)   # allow everything to be written to
-br.set_handle_robots(False)   # no robots
-br.set_handle_refresh(True)   # can sometimes hang without this
-br.set_handle_redirect(True)
-
-br.add_password(url, user, password) # nos autentificamos
-br.addheaders = [
-    ('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
-    #('Referer', referer)
-    ]
+natas = libnatas.NatasBrowser(level=6, password=password)
 
 # de index-source.html se obtiene la url al script que valida la password
 # el $secret esta en la siguiente url
 script = 'includes/secret.inc'
-html_response = br.open(url + '/' + script)
-soup = BeautifulSoup(html_response)
+soup = natas.get_html_soup(natas.level_url + '/' + script)
+
 # regex para extraer la password:
 regex1 = re.compile('.*"(.*)".*')
 secret = regex1.findall(soup.prettify())[0]
-
 #print secret
 
-
 # reabrimos la pagina principal y rellenamos el formulario
-br.open(url)
-br.select_form(nr=0) # primera form (y unica)
-br.form['secret'] = secret
+natas.open(natas.level_url)
+natas.select_form(nr=0) # primera form (y unica)
+natas.form['secret'] = secret
 
-html_response = br.submit()
-
-soup = BeautifulSoup(html_response)
-
+html = natas.submit()
 
 regex = re.compile('.*natas7 is (.*)\W.*')
-password_nl = regex.findall(soup.prettify())[0]
-print "Password for level natas%s is %s" % (nextlevel, password_nl)
+password_nl = regex.findall(html.read())[0]
+natas.print_password(password_nl)
